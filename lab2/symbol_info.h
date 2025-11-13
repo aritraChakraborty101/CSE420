bb4586248a9d25993b6d0b13ccebc3bf37fea350
mehedialif002 <mhalif002@gmail.com>
2025-11-14 03:21:57 +0600

    symbolinfo

--- lab2/symbol_info.h
+++ lab2/symbol_info.h
@@ -1,43 +1,97 @@
-#include<bits/stdc++.h>
-using namespace std;
-
 class symbol_info
 {
 private:
-    string name;
-    string type;
-
+    string idname;
+    string datatype;
+    SymbolCategory category;
+    vector<SymbolInfo*>paramList;
+    int arrLength;
     // Write necessary attributes to store what type of symbol it is (variable/array/function)
     // Write necessary attributes to store the type/return type of the symbol (int/float/void/...)
     // Write necessary attributes to store the parameters of a function
     // Write necessary attributes to store the array size if the symbol is an array
 
 public:
-    symbol_info(string name, string type)
-    {
-        this->name = name;
-        this->type = type;
+    symbol_info(string id=" ", string dtype=" ",SymbolCategory c=SymbolCategory::IDENTIFIER,const vector<SymbolInfo*>& params={},int length=-1):idName(id),dataType(dCOtype),category(c),arrLength(length){
+        if (c==SymbolCategory::FUNCTION_TYPE)
+        {
+        paramList = params;
+        }
     }
-    string get_name()
-    {
-        return name;
-    }
-    string get_type()
-    {
-        return type;
-    }
-    void set_name(string name)
-    {
-        this->name = name;
-    }
-    void set_type(string type)
-    {
-        this->type = type;
-    }
-    // Write necessary functions to set and get the attributes
 
-    ~symbol_info()
+    
+        SymbolInfo(const SymbolInfo& other)
     {
-        // Write necessary code to deallocate memory, if necessary
+        idName = other.idName;
+        dataType = other.dataType;
+        category = other.category;
+        arrLength = other.arrLength;
+
+        // Deep copy of parameter list
+        for (auto* p : other.paramList)
+        {
+            paramList.push_back(new SymbolInfo(*p));
+        }
+    }
+
+    
+    string getIdentifier() 
+    const { 
+        return idName;
+     }
+    string getDataType() 
+    const { 
+        return dataType; 
+    }
+    SymbolCategory getCategory() 
+    const { 
+        return category;
+     }
+    int getArrayLength() 
+    const { 
+        return arrLength; 
+    }
+
+    
+
+  void setIdentifier(const string& id) { idName = id; }
+    void setDataType(const string& dtype) { dataType = dtype; }
+    void setCategory(SymbolCategory cat) { category = cat; }
+    void setArrayLength(int len) { arrLength = len; }
+
+    
+    void setParameters(const vector<SymbolInfo*>& params)
+    {
+        paramList = params;
+    }
+
+    const vector<SymbolInfo*>& getParameters() const
+    {
+        return paramList;
+    }
+
+    void addParameter(SymbolInfo* param)
+    {
+        paramList.push_back(param);
+    }
+
+    
+    bool isVariable() 
+    const { 
+        return category == SymbolCategory::IDENTIFIER; }
+    bool isArray() 
+    const { 
+        return category == SymbolCategory::ARRAY_TYPE; }
+    bool isFunction() 
+    const { return category == SymbolCategory::FUNCTION_TYPE; }
+
+    
+    ~SymbolInfo()
+    {
+        for (auto* param : paramList)
+        {
+            delete param;
+        }
+        paramList.clear();
     }
 };
